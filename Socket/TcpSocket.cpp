05954d866b2492a56a4a4ab107347c1bcf3fbd24
Mevaseret <mevas376@gmail.com>
2020-01-16 22:36:35 +0200

    Add TcpSocket class, with rule of 5

--- Socket/TcpSocket.cpp
+++ Socket/TcpSocket.cpp
@@ -2,4 +2,42 @@
 // Created by mevaseret on 16/01/2020.
 //
 
+#include <utility>
+
+#include <unistd.h>
+
 #include "TcpSocket.h"
+
+namespace utilities {
+    namespace sockets {
+
+        TcpSocket::TcpSocket(TcpSocket &&rhs) noexcept {
+            *this = std::move(rhs);
+        }
+
+        TcpSocket &TcpSocket::operator=(TcpSocket &&rhs) noexcept {
+            if (&rhs == this) {
+                return *this;
+            }
+
+            m_socketFd = rhs.m_socketFd;
+            rhs.m_socketFd = -1;
+            return *this;
+        }
+
+        TcpSocket::TcpSocket(int socketFd) noexcept :
+                m_socketFd(socketFd) {
+        }
+
+        TcpSocket::~TcpSocket() {
+            try {
+                if (-1 != m_socketFd) {
+                    close(m_socketFd);
+                }
+            }
+            catch (...) {
+
+            }
+        }
+    }
+}
--- Socket/TcpSocket.h
+++ Socket/TcpSocket.h
@@ -8,6 +8,21 @@ namespace utilities {
     namespace sockets {
         class TcpSocket {
 
+        public:
+            explicit TcpSocket(int socketFd) noexcept;
+
+            TcpSocket(const TcpSocket &) = delete;
+
+            TcpSocket &operator=(const TcpSocket &) = delete;
+
+            TcpSocket(TcpSocket &&rhs) noexcept;
+
+            TcpSocket &operator=(TcpSocket &&rhs) noexcept;
+
+            ~TcpSocket();
+
+        private:
+            int m_socketFd = -1;
         };
     }
 }
