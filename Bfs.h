f2cebc5557e0be5776af9e0dc820fa0e6d3ea71c
Shani Cattan <323shani@gmail.com>
2020-01-24 10:45:48 +0200

    Removed redefinitions of graph

--- Bfs.h
+++ Bfs.h
@@ -6,17 +6,3 @@
 
 #include <vector>
 
-// This class represents a directed graph using
-// adjacency list representation
-class Graph
-{
-public:
-
-    virtual void addEdge(int v, int w) = 0;
-
-    virtual bool areConnected(int v1, int v2) = 0;
-
-    virtual ~Graph() = default;
-};
-
-
